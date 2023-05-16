b192142ea47fcfe89822d31539f5fe34830013c4
Pawel <pakajumper@gmail.com>
2023-05-16 17:29:32 +0200

    wrzucamy wszystko

---
+++ odpowiedzi/pojedyncze/zadanie1/gra/insert.php
@@ -0,0 +1,29 @@
+<?php
+$host = 'localhost';
+$db = 'baza';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = $conn->query("SELECT * FROM score");
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    // Pobranie wyników zapytania do tablicy asocjacyjnej
+
+    foreach ($result as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>"; // Wyświetlenie wartości kolumny
+        echo "<td>" . $row['creation_time'] . "</td>";
+        echo "<td>" . $row['modification_time'] . "</td>";
+        echo "<td>" . $row['deleted'] . "</td>";
+        echo "<td>" . $row['game_id'] . "</td>";
+        echo "<td>" . $row['player_id'] . "</td>";
+        echo "<td>" . $row['value'] . "</td>";
+        echo "</tr>";
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
