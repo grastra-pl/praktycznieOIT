fe062213abe50799fed89de5cbfc2f957b651ec6
Kacper Zapart <k4p1email@gmail.com>
2023-05-25 14:20:08 +0200

    dodanie polaczenia z baza, selekcji z bazy izwrotk

--- zespol_backend/public_html/api.php
+++ zespol_backend/public_html/api.php
@@ -1,3 +1,28 @@
 <?php
+include('credentials.php');
 
-die('Tu jest miejsce na utworzenie Waszej aplikacji backendowej');
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $pdo = new PDO($dsn, $user, $password);
+    $jsonData = file_get_contents('php://input');
+    if (!$jsonData) {
+        die("błąd wysyłania");
+    }
+    $data = json_decode($jsonData, true);
+    $yourValue = $data['rawValue'];
+    if(!is_int($yourValue)){
+        die("wystąpił błąd");
+    }
+    $myValue = getUserData($pdo,$yourValue);
+    if(!$myValue){
+        $arrayResult = array('yourValue' => $yourValue);
+        echo(json_encode($yourValue));
+    }
+
+    $arrayResult = array('yourValue' => $yourValue, 'myValue' => $myValue);
+    echo(json_encode($arrayResult));
+}
+function getUserData($pdo, $number) {
+    $stmt = $pdo->prepare("SELECT raw_value FROM user WHERE raw_value>=? ORDER BY raw_value DESC LIMIT 1");
+    $stmt->execute([$number]);
+    return $stmt->fetch();
+}
---
+++ zespol_backend/public_html/credentials.php
@@ -0,0 +1,8 @@
+<?php
+$host='localhost';
+$db='database1';
+$charset='UTF-8';
+$port='3306';
+$user='root';
+$password='';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
