25c67e20476b7ddcde3b9d36f739e5f63e257b9d
Kacper Zapart <k4p1email@gmail.com>
2023-05-25 13:26:19 +0200

    1

--- zespol_02/index.php
+++ zespol_02/index.php
@@ -1,11 +1,12 @@
 <?php
 $jsonData = file_get_contents('php://input');
 if ($jsonData) {
+    
     $data = json_decode($jsonData, true);
 
     $id = $data['id'];
 
     $result = $id * 2;
-
+    echo(is_int($id));
     echo $result;
 }
