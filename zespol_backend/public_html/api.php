7d7b7ddec99b8738400a9658618496732177aa3a
Pawel <pakajumper@gmail.com>
2023-05-26 12:10:26 +0200

    łączenie

--- zespol_backend/public_html/api.php
+++ zespol_backend/public_html/api.php
@@ -1,5 +1,9 @@
 <?php
 include ('credentials.php');
-
-
-
+try{
+    $pdo = new PDO('mysql:host=' + $servername + ';dbname=' + $database_name + ';port=' + $port + ';charset='+$charset+, $username, $password );
+    echo('<p>Houston mamy połączenie!</p>');
+  }catch(PDOException $e){
+    echo('<p>Houston mamy problem! Nie możemy się połączyć!</p>');
+    die(); // Nie połączyłeś się? To nie ma co robić nic więcej!
+  }
